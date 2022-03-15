e3dcdec5591cfd383dc7b495be162a07df39df96
MeryMacias <ada_hack212@hackademy.mx>
2022-03-14 19:17:34 -0600

    cambio

--- Actividad5/ejercicio2.js
+++ Actividad5/ejercicio2.js
@@ -1,4 +1,4 @@
-var numeros = [1,2,3,4,5];
+/*var numeros = [1,2,3,4,5];
 var index = 0;
 while (index < numeros.length){
     
@@ -6,4 +6,16 @@ while (index < numeros.length){
 
         index++;
     
-};
+};*/
+
+
+
+var arreglo =[]
+
+var i = 0
+while( i < 5){
+        arreglo [i] = i
+        i++
+}
+
+console.log (arreglo)
